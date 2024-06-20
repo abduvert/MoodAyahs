ea1a8edd562c19fa40892f559ccccbfeee8164b7
abduvert <sajjadabdullah345@gmail.com>
2024-06-21 02:48:37 +0500

    Minor changes

--- src/components/button.jsx
+++ src/components/button.jsx
@@ -1,3 +1,5 @@
+import '../Button.css'
+
 const Button = ({children,onClick,style}) =>{
     return (<div className='main'>
         <button className="btn btn-primary rounded-pill" onClick={onClick} style={style}>
